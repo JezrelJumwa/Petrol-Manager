c15540ba407290bf1b818e3aaa8b1144d5fdd11d
Yuliyan Lyubenov <yuliyan.lyubenov@stanga.net>
2017-06-05 11:58:28 +0300

    Removed the old retrofit 1.9 and updated the adapter implementation to use it's own callback interface which is not dependent on the the network layer

--- app/src/main/java/com/sstgroup/xabaapp/network/mock/NetworkMockService.java
+++ app/src/main/java/com/sstgroup/xabaapp/network/mock/NetworkMockService.java
@@ -1,5 +1,6 @@
 package com.sstgroup.xabaapp.network.mock;
 
+import com.sstgroup.xabaapp.network.adapter.AdapterCallback;
 import com.sstgroup.xabaapp.network.adapter.objects.CountryModel;
 import com.sstgroup.xabaapp.network.adapter.objects.IndustryModel;
 import com.sstgroup.xabaapp.network.adapter.objects.ProfileModel;
@@ -10,50 +11,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Header;
-import retrofit.client.Response;
-import retrofit.mime.TypedInput;
-
 /**
  * Created by julianlubenov on 5/10/17.
  */
 
 public class NetworkMockService {
 
-    private Response getMockResponse() {
-        return new Response("login", 200, "ok", new ArrayList<Header>(), new TypedInput() {
-            @Override
-            public String mimeType() {
-                return null;
-            }
-
-            @Override
-            public long length() {
-                return 0;
-            }
-
-            @Override
-            public InputStream in() throws IOException {
-                return null;
-            }
-        });
+    public void login(String pin, String nationalId, AdapterCallback<ProfileModel> callback) {
+        callback.onResponse(new ProfileModel(), "", 200);
     }
 
-    public void login(String pin, String nationalId, Callback<ProfileModel> callback) {
-        callback.success(new ProfileModel(), getMockResponse());
-    }
-
-    public void countries(String hash, Callback<ArrayList<CountryModel>> callback) {
+    public void countries(String hash, AdapterCallback<ArrayList<CountryModel>> callback) {
         ArrayList<CountryModel> countries = new ArrayList<CountryModel>();
         countries.add(new CountryModel());
-        callback.success(countries, getMockResponse());
+        callback.onResponse(countries, "", 200);
     }
 
-    public void industries(String hash, Callback<ArrayList<IndustryModel>> callback) {
+    public void industries(String hash, AdapterCallback<ArrayList<IndustryModel>> callback) {
         ArrayList<IndustryModel> industries = new ArrayList<IndustryModel>();
         industries.add(new IndustryModel());
-        callback.success(industries, getMockResponse());
+        callback.onResponse(industries, "", 200);
     }
 }
