b2bce47524bd8a9ef1a9906bd921de5957322ab4
Rosen Stoyanov <rosen.stoyanov@stanga.net>
2017-06-23 09:21:24 +0300

    add null check in login

--- app/src/main/java/com/sstgroup/xabaapp/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/sstgroup/xabaapp/ui/fragments/LoginFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.sstgroup.xabaapp.BuildConfig;
 import com.sstgroup.xabaapp.R;
 import com.sstgroup.xabaapp.XabaApplication;
 import com.sstgroup.xabaapp.models.User;
@@ -61,10 +60,10 @@ public class LoginFragment extends BaseFragment {
 //        }
 
         //user with notification data
-        if (BuildConfig.DEBUG) {
-            mEditTextNationalId.setText("235720441");
-            mEditTextPinCode.setText("0000");
-        }
+//        if (BuildConfig.DEBUG) {
+//            mEditTextNationalId.setText("235720441");
+//            mEditTextPinCode.setText("0000");
+//        }
     }
 
     @Override
