04f78fd149e6ba4dab5915a1c9089d7676c5418a
Yuliyan Lyubenov <yuliyan.lyubenov@stanga.net>
2017-07-04 17:42:23 +0300

    Added tests for edit profile, selecting programs and professions.

--- app/src/androidTest/java/com/sstgroup/xabaapp/ui/activities/EditProfileActivityTest.java
+++ app/src/androidTest/java/com/sstgroup/xabaapp/ui/activities/EditProfileActivityTest.java
@@ -1,10 +1,10 @@
-package com.sstgroup.xabaapp.ui.fragments;
+package com.sstgroup.xabaapp.ui.activities;
 
+import android.content.Intent;
 import android.support.test.espresso.FailureHandler;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -13,11 +13,7 @@ import android.widget.TextView;
 import com.sstgroup.xabaapp.FeedbackMatcher;
 import com.sstgroup.xabaapp.R;
 import com.sstgroup.xabaapp.WaitAction;
-import com.sstgroup.xabaapp.ui.activities.SplashActivity;
-
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static com.sstgroup.xabaapp.Helpers.first;
-import static com.sstgroup.xabaapp.Helpers.viewHolderAtPosition;
+import com.sstgroup.xabaapp.XabaApplication;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -26,47 +22,53 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.sstgroup.xabaapp.Helpers.first;
+import static com.sstgroup.xabaapp.Helpers.viewHolderAtPosition;
 import static com.sstgroup.xabaapp.WaitAction.waitFor;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.*;
 
 /**
- * Created by julianlubenov on 6/30/17.
+ * Created by julianlubenov on 7/4/17.
  */
-@RunWith(AndroidJUnit4.class)
-public class RegisterWorkerAgentFragmentTest {
+public class EditProfileActivityTest {
 
     @Rule
-    public ActivityTestRule<SplashActivity> mActivityRule =
-            new ActivityTestRule(SplashActivity.class);
+    public ActivityTestRule<LoginActivity> mActivityRule =
+            new ActivityTestRule(LoginActivity.class, true, false);
+
+    @Rule
+    public ActivityTestRule<EditProfileActivity> mEditProfileRule = new ActivityTestRule<EditProfileActivity>(EditProfileActivity.class, true, false);
 
     @Before
     public void setUp() throws Exception {
+        XabaApplication.getInstance().logout();
 
+        mActivityRule.launchActivity(new Intent());
     }
 
     @After
     public void tearDown() throws Exception {
-
+        XabaApplication.getInstance().logout();
     }
 
     @Test
-    public void testRegisterScreenDisplaysListOfPrograms() {
+    public void testUpdateProfileScreenDisplaysListOfPrograms() {
         //wait for the spalsh screen to disappear
 
         onView(withId(R.id.register)).withFailureHandler(new FailureHandler() {
@@ -76,28 +78,56 @@ public class RegisterWorkerAgentFragmentTest {
                 onView(isRoot()).perform(waitFor(1000));
             }
         });
-        enterRegister();
+        enterUpdateProfile();
         checkProgramsSelection();
+    }
+
+    private void enterUpdateProfile() {
+        onView(withId(R.id.national_id)).perform(replaceText("22330828"));
+        onView(withId(R.id.pin_code)).perform(replaceText("1234"));
+        onView(withId(R.id.log_in)).perform(click());
+
+        Intent updateProfileIntent = new Intent();
+        mEditProfileRule.launchActivity(updateProfileIntent);
+    }
+
+    public static void checkProgramsSelection() {
+
+        onView(withId(R.id.rv_edit_profile)).perform(RecyclerViewActions.actionOnItemAtPosition(5,scrollTo()));
+        onView(withId(R.id.row_profile_frame)).perform(click());
+        //Check that the chooser dialog is displayed
+        onView(withId(R.id.rv_chooser)).check(matches(isDisplayed()));
+        FeedbackMatcher feedback = new FeedbackMatcher(TextView.class);
+
+        onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), isDescendantOfA(viewHolderAtPosition(3)), first(feedback))).perform(click());
+        onView(withId(R.id.btn_close)).perform(click());
+
+        onView(withId(R.id.txt_profession)).check(matches(withText(feedback.getFeedbackText())));
+
+        Log.d("Pass" , "feeback: " + feedback.getFeedbackText());
+
     }
 
     @Test
     public void testGenericProfessionsDifferForDifferentCategories() {
-        enterRegister();
+        enterUpdateProfile();
 
-        onView(withId(R.id.grp_industry)).perform(scrollTo());
+        onView(withId(R.id.rv_edit_profile)).perform(RecyclerViewActions.actionOnItemAtPosition(5,scrollTo()));
+
+        onView(withId(R.id.remove_profession_one)).perform(click());
+        onView(withId(R.id.add_another_profession)).perform(click());
+
+        onView(withId(R.id.rv_edit_profile)).perform(RecyclerViewActions.actionOnItemAtPosition(5,scrollTo()));
+
         onView(withId(R.id.grp_industry)).perform(click());
         onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), withText(containsString("Agriculture")))).perform(click());
-        onView(isRoot()).perform(waitFor(100));
         onView(withId(R.id.txt_industry_selection)).check(matches(withText("Agriculture")));
 
 
-        onView(withId(R.id.grp_category)).perform(scrollTo());
         onView(withId(R.id.grp_category)).perform(click());
         onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), withText(containsString("General")))).perform(click());
-        onView(isRoot()).perform(waitFor(100));
         onView(withId(R.id.txt_category_selection)).check(matches(withText("General")));
 
-        onView(withId(R.id.grp_profession)).perform(scrollTo());
         onView(withId(R.id.grp_profession)).perform(click());
         final ArrayList<String> professions1 = new ArrayList<>();
         final ArrayList<String> professions2 = new ArrayList<>();
@@ -124,22 +154,16 @@ public class RegisterWorkerAgentFragmentTest {
             }
         }));
         onView(withId(R.id.rv_chooser)).perform(actionOnItemAtPosition(0, click()));
-        onView(isRoot()).perform(waitFor(100));
 
-        onView(withId(R.id.grp_industry)).perform(scrollTo());
         onView(withId(R.id.grp_industry)).perform(click());
         onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), withText(containsString("Domestic")))).perform(click());
-        onView(isRoot()).perform(waitFor(100));
         onView(withId(R.id.txt_industry_selection)).check(matches(withText("Domestic")));
 
 
-        onView(withId(R.id.grp_category)).perform(scrollTo());
         onView(withId(R.id.grp_category)).perform(click());
         onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), withText(containsString("General")))).perform(click());
-        onView(isRoot()).perform(waitFor(100));
         onView(withId(R.id.txt_category_selection)).check(matches(withText("General")));
 
-        onView(withId(R.id.grp_profession)).perform(scrollTo());
         onView(withId(R.id.grp_profession)).perform(click());
 
         onView(withId(R.id.rv_chooser)).check(matches(new BoundedMatcher<View, View>(View.class) {
@@ -183,31 +207,4 @@ public class RegisterWorkerAgentFragmentTest {
 
     }
 
-    public static void enterRegister() {
-        onView(withId(R.id.grp_country)).perform(click());
-        onView(withId(R.id.rv_chooser)).perform(click());
-
-        onView(withId(R.id.grp_language)).perform(click());
-        onView(withId(R.id.rv_chooser)).perform(click());
-
-        onView(withId(R.id.register)).perform(click()).check(matches(not(isDisplayed())));
-    }
-
-    public static void checkProgramsSelection() {
-
-        onView(withId(R.id.grp_program)).check(matches(isDisplayed()));
-        onView(withId(R.id.grp_program)).perform(scrollTo());
-        onView(withId(R.id.grp_program)).perform(click());
-        //Check that the chooser dialog is displayed
-        onView(withId(R.id.rv_chooser)).check(matches(isDisplayed()));
-        FeedbackMatcher feedback = new FeedbackMatcher(TextView.class);
-
-        onView(allOf(isDescendantOfA(withId(R.id.rv_chooser)), isDescendantOfA(viewHolderAtPosition(3)), first(feedback))).perform(click());
-        onView(withId(R.id.btn_close)).perform(click());
-
-        onView(withId(R.id.txt_program)).check(matches(withText(feedback.getFeedbackText())));
-
-        Log.d("Pass" , "feeback: " + feedback.getFeedbackText());
-
-    }
 }
